9b57147b67c9bab1a54b7bd4847e496806a9cc07
tyler-e <teisenbraun27@gmail.com>
2024-03-10 17:00:57 -0400

    better
    better

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<View style={styles.btnContainer}>
+        <TouchableOpacity style={{ height: 110, marginTop: 10, alignItems: 'center', backgroundColor: "gray", justifyContent: 'center'}} onPress={() => this.props.navigation.navigate('Home')}>
+          <Text>Mind</Text>
+        </TouchableOpacity>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
+  buttonContainer: {
+    margin: 5,
+ 
+  },
+  red: {
+    color: 'red',
+  },
+  btnContainer:{
+    justifyContent:"space-between"
+  },
+
 });
